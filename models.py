04d44531324f58260726be8cab69dbc7c19ff625
Лан <128209597+UristAnarchist@users.noreply.github.com>
2023-07-03 22:23:51 +0900

    Add files via upload

---
+++ models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    author_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating_author = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        post_rate = self.post_set.aggregate(post_rating=Sum('rating'))
+        current_post_rate = 0
+        current_post_rate += post_rate.get('post_rating')
+
+        author_comment_rate = self.author_user.comment_set.aggregate(comment_rating=Sum('rating'))
+        current_post_comment_rate = 0
+        current_post_comment_rate += author_comment_rate.get('comment_rating')
+
+        post_comment_rate = self.comment_user.comment_set.aggregate(post_comment_rating=Sum('rating'))
+        current_post_comment_rate = 0
+        current_post_comment_rate += post_comment_rate.get('post_comment_rating')
+
+        self.rating_author = current_post_rate * 3 + current_post_comment_rate + current_post_comment_rate
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    NEWS = 'NW'
+    ARTICLE = "AR"
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья')
+    )
+    category_type = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    date = models.DateTimeField(auto_now_add=True)
+    post_category = models.ManyToManyField(Category, through="PostCategory")
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def preview(self):
+        return f"{self.text[:123]}..."
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post_through = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category_through = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
